020d5052c20e32b56fcfe6356646c9babd8739ef
jespinozap <je1104079@gmail.com>
2025-06-27 14:21:38 -0600

    Solucion conflicto list
    Se acomodaron los metodos add y find

--- src/Lists/List.java
+++ src/Lists/List.java
@@ -6,8 +6,10 @@ package Lists;
 
 /**
  *
- * @author samue
+<<<<<<< HEAD
+ * @author samue, jespinoza
  */
 public interface List <T> {
     public boolean add(T t);
+    public T find(Object id);
 }
