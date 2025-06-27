e55410a5c81dff95bd9802dd956121c06a0524d8
Brandon <brandonvaldelomar8@gmail.com>
2025-06-27 13:54:10 -0600

    Se crea el paquete y clase Lists

---
+++ src/Lists/List.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author Brandon Valdelomar
+ */
+public interface List <T> {
+    
+    public boolean remove (T t);
+    
+}
