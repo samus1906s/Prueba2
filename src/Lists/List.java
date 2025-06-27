1d018a174d918813e4be4e84f76b96f9ba66280e
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-06-27 13:55:28 -0600

    Clase List ShowAll

---
+++ src/Lists/List.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author Eduard Salas Murillo
+ */
+public interface List <T> {
+
+public void showAll();    
+}
