7401848664d6f5b1ebd846d03af925fea7b0dadc
samus <samuelcedas19@gmail.com>
2025-06-27 13:40:08 -0600

    Clase List

---
+++ src/Lists/List.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author samue
+ */
+public interface List {
+    
+}
